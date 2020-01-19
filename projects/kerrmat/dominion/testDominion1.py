0a8a50c08ff97a609de3b8c67ece943f53a68560
Matthew Kerr <kerrmat@oregonstate.edu>
2020-01-19 14:44:10 -0800

    Added Bugs

---
+++ projects/kerrmat/dominion/testDominion1.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 19 1:29 2020
+
+@author: mkerr
+"""
+
+import Dominion
+import testUtility
+import random
+from collections import defaultdict
+
+print("Testing")
+
+#Get player names
+player_names = ["Annie","*Ben","*Carla"]
+
+#number of curses and victory cards
+if len(player_names)>2:
+    nV=12
+else:
+    nV=8
+nC = -10 + 10 * len(player_names)
+
+#Define box
+box = testUtility.getBoxes(nV)
+
+supply_order = {0:['Curse','Copper'],2:['Estate','Cellar','Chapel','Moat'],
+                3:['Silver','Chancellor','Village','Woodcutter','Workshop'],
+                4:['Gardens','Bureaucrat','Feast','Militia','Moneylender','Remodel','Smithy','Spy','Thief','Throne Room'],
+                5:['Duchy','Market','Council Room','Festival','Laboratory','Library','Mine','Witch'],
+                6:['Gold','Adventurer'],8:['Province']}
+
+#Pick 10 cards from box to be in the supply.
+supply = testUtility.getSupply(box, player_names, nV, nC)
+supply["Silver"]=[Dominion.Silver()]*1000
+
+
+#initialize the trash
+trash = []
+
+#Costruct the Player objects
+players = testUtility.getPlayers(player_names)
+
+
+#Play the game
+turn  = 0
+while not Dominion.gameover(supply):
+    turn += 1    
+    print("\r")    
+    for value in supply_order:
+        print (value)
+        for stack in supply_order[value]:
+            if stack in supply:
+                print (stack, len(supply[stack]))
+    print("\r")
+    for player in players:
+        print (player.name,player.calcpoints())
+    print ("\rStart of turn " + str(turn))    
+    for player in players:
+        if not Dominion.gameover(supply):
+            print("\r")
+            player.turn(players,supply,trash)
+            
+
+#Final score
+dcs=Dominion.cardsummaries(players)
+vp=dcs.loc['VICTORY POINTS']
+vpmax=vp.max()
+winners=[]
+for i in vp.index:
+    if vp.loc[i]==vpmax:
+        winners.append(i)
+if len(winners)>1:
+    winstring= ' and '.join(winners) + ' win!'
+else:
+    winstring = ' '.join([winners[0],'wins!'])
+
+print("\nGAME OVER!!!\n"+winstring+"\n")
+print(dcs)
